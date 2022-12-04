a4417d023d2deb729487666692978e79360b6359
bramoo <brad.moorfield@gmail.com>
2022-12-04 14:35:29 +1300

    Add historic keymaps

---
+++ preonic/config.h
@@ -0,0 +1,8 @@
+// Configure the globL tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Hold key within tapping term if keypress is nested
+#define PERMISSIVE_HOLD
